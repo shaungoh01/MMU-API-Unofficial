eedc2ab4c81851f2dff84d71d5ead8eec99932b8
Shaun <shaungoh01@gmail.com>
2015-06-28 09:59:19 +0800

    added title function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def about
+  	@title = "About"
   end
 
   def contact
   end
 
   def api
+  	@tilte = "API"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def tilte
+		base_title = "MMU HUB"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} - #{@title}"
+		end
+	end	
 end
